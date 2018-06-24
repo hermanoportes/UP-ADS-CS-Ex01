3e10931a3681249e027fecd9caaa23ed07d95c2d
hermanoportes <hermanoportes@hotmail.com>
2018-06-24 12:17:09 -0300

    Criação da estrutura MVC

---
+++ ConsoleView/ConsoleView/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vai se Foder via Git");
+            Console.ReadKey();
+        }
+    }
+}
