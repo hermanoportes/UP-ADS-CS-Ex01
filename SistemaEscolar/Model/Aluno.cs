9146bb30f87a21b1854ae035e46f55c1b752087f
hermanoportes <hermanoportes@hotmail.com>
2018-06-25 12:03:52 -0300

    Atualização pastas e nomes.

---
+++ SistemaEscolar/Model/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Aluno
+    {
+        private int matricula;
+        private string nome;
+
+        public int Matricula { get => matricula; set => matricula = value; }
+        public string Nome { get => nome; set => nome = value; }
+    }
+}
